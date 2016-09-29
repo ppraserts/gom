dafa2a4c5cbc5a2aff07f2d99cd57d229d0e7783
pprasert <tae_pe@hotmail.com>
2016-09-29 08:36:16 +0700

    ContactUs Form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'admin/home';
+    protected $redirectTo = 'admin/productcategory';
 
     /**
      * Create a new controller instance.
