6661462f375340f260ef4a6b875a05ee56c3f026
Paul <ppraserts@gmail.com>
2017-05-06 11:57:29 +0700

    Add enable FK

--- laravel/database/migrations/2017_05_05_060358_change_column_product_id_to_product_request_id_in_order_itmes_table.php
+++ laravel/database/migrations/2017_05_05_060358_change_column_product_id_to_product_request_id_in_order_itmes_table.php
@@ -37,7 +37,7 @@ class ChangeColumnProductIdToProductRequestIdInOrderItmesTable extends Migration
             }
 
         });
-        Schema::disableForeignKeyConstraints();
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
