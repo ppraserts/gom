10080288a99a6c560ef2260c742085be60ac4d4c
sawang <sawang1990@gmail.com>
2017-06-15 16:41:32 +0700

    fix type order

--- laravel/resources/views/frontend/orderindex.blade.php
+++ laravel/resources/views/frontend/orderindex.blade.php
@@ -34,6 +34,7 @@ $pagetitle = trans('message.menu_order_list');
                     <thead>
                     <tr>
                         <th width="120px" style="text-align:center;">{{ trans('messages.order_id') }}</th>
+                        <th style="text-align:center;">{{ trans('messages.order_type') }}</th>
                         <th>{{ trans('messages.i_sale') }}</th>
                         <th style="text-align:center;">{{ trans('messages.order_date') }}</th>
                         <th style="text-align:center;">{{ trans('messages.order_total') }}</th>
@@ -47,6 +48,9 @@ $pagetitle = trans('message.menu_order_list');
                     @foreach ($orderList as $key => $item)
                         <tr>
                             <td style="text-align:center;">{{ $item->id }}</td>
+                            <td style="text-align:center;">
+                                {{ $item->order_type== 'retail'? trans('messages.retail'): trans('messages.wholesale')}}
+                            </td>
                             <td>{{ $item->users_firstname_th. " ". $item->users_lastname_th }}</td>
                             <td style="text-align:center;">{{ $item->order_date }}</td>
                             <td style="text-align:center;">{{ $item->total_amount . trans('messages.baht') }}</td>
