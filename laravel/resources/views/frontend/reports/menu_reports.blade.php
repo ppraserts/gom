a3c7a612a3bcae05ea75f1afc0e1eb6c33ad2c0a
sawang <sawang1990@gmail.com>
2017-07-05 13:48:55 +0700

    - check user permission report menu

--- laravel/resources/views/frontend/reports/menu_reports.blade.php
+++ laravel/resources/views/frontend/reports/menu_reports.blade.php
@@ -1,11 +1,18 @@
+<?php
+$user = auth()->guard('user')->user();
+?>
 <ul class="nav nav-tabs">
+    @if($user->iwanttobuy == "buy")
     <li @if(Request::segment(3) == 'buy') class="active" @endif>
         <a href="{{url('user/reports/buy')}}">{{ trans('messages.menu_order_list') }}</a>
     </li>
+    @endif
+    @if($user->iwanttosale == "sale")
     <li @if(Request::segment(3) == 'list-sale') class="active" @endif>
         <a href="{{url('user/reports/list-sale')}}">{{ trans('messages.menu_shop_order_list') }}</a>
     </li>
     <li  @if(Request::segment(3) == 'sale') class="active" @endif>
         <a href="{{url('user/reports/sale')}}">รายงานยอดจำหน่ายสินค้า</a>
     </li>
+    @endif
 </ul>
