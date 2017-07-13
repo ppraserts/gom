8aac7d21fc8769c4b97e549b23b0e0bdc6bb4bbe
sawang <sawang1990@gmail.com>
2017-07-13 23:29:00 +0700

    - fix user menu report

--- laravel/resources/views/shared/usermenu.blade.php
+++ laravel/resources/views/shared/usermenu.blade.php
@@ -86,6 +86,8 @@ $productRequest = new ProductRequest();
                 {{ trans('messages.menu_promotion') }}
             </a>
         </li>
+    @endif
+    @if($user->iwanttosale == "sale" or $user->iwanttobuy == "buy")
         <li role="presentation" {{ (Request::segment(2) == "reports")? 'class=active' : ''  }}>
             <a href="{{ url('/user/reports/buy') }}">
                 รายงาน
