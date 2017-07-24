19719ae167d9a1aa0c8be39a257ed50aef06fb89
layer <layerlre@gmail.com>
2017-07-24 18:09:02 +0700

    fix user report

--- laravel/resources/views/frontend/reports/menu_reports.blade.php
+++ laravel/resources/views/frontend/reports/menu_reports.blade.php
@@ -4,12 +4,12 @@ $user = auth()->guard('user')->user();
 <ul class="nav nav-tabs">
     @if($user->iwanttobuy == "buy")
     <li @if(Request::segment(3) == 'buy') class="active" @endif>
-        <a href="{{url('user/reports/buy')}}">{{ trans('messages.menu_order_list') }}</a>
+        <a href="{{url('user/reports/buy')}}">{{ trans('messages.report_order') }}</a>
     </li>
     @endif
     @if($user->iwanttosale == "sale")
     <li @if(Request::segment(3) == 'list-sale') class="active" @endif>
-        <a href="{{url('user/reports/list-sale')}}">{{ trans('messages.menu_shop_order_list') }}</a>
+        <a href="{{url('user/reports/list-sale')}}">{{ trans('messages.report_sale') }}</a>
     </li>
     <li  @if(Request::segment(3) == 'sale') class="active" @endif>
         <a href="{{url('user/reports/sale')}}">{{trans('messages.report_title_sale')}}</a>
